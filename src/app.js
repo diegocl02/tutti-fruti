ea3beb4a01bd17735d7a7ae1a6415eb6819c8ada
Jesus <jesus.reyponce@gmail.com>
2019-01-23 21:09:38 -0500

    added server.js config for socket.io, and minimal updates to other files

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import io from "socket.io-client";
 
 const styles = {
   app: {
     paddingTop: 40,
-    textAlign: 'center',
-  },
-}
+    textAlign: "center"
+  }
+};
 
 export class App extends Component {
   render() {
-    return (
-      <div style={styles.app}>
-        Welcome to React!
-      </div>
-    )
+    return <div style={styles.app}>Welcome to React!</div>;
+  }
+
+  componentDidMount() {
+    const socket = io("http://localhost:3000");
+    // socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+
+  componentWillUnmount() {
+    // socket.disconnect();
   }
 }
